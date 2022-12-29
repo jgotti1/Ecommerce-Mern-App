20c1eca020dcfa1816c43d4acda000f8de53cb09
John Margotti <jgotti1@optonline.net>
2022-12-29 13:12:27 -0500

    fronted work homepage and products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+
 function App() {
   return (
-    <>
-      <h1>Welcome to ProShop Online</h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
